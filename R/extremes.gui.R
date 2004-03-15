660c4adb105be746c565f199e9b13133868eeb96
Eric Gilleland <ericg@ucar.edu>
2004-03-15 00:00:00 +0000

    version 1.59

--- R/extremes.gui.R
+++ R/extremes.gui.R
@@ -251,7 +251,7 @@ gpdsimdata <- function() {
 	} # end of fitsummary fcn
 
     base <- tktoplevel()
-    tkwm.title(base, "Extremes Toolkit: version 1.58")
+    tkwm.title(base, "Extremes Toolkit: version 1.59")
     top.frm <- tkframe(base, borderwidth = 2)
     bottom.frm <- tkframe(base, borderwidth = 2)
 
